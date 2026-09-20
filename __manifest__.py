c2391ec68740d3347244b429e9036197036cca0d
Sanjaya <sanjaya.w@jinasena.com.lk>
2026-09-20 15:57:08 +0530

    Initial: master-data extracted from CDB (2026-09-20)

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Jinasena : MasterData : Common',
+    'version': '17.0.0.0.1',
+    'summary': 'Master-data extracted from CDB for Common domain.',
+    'description': 'Extracted from Clear-DB. Test-env master data. Edit the CSVs in data/ to add/remove rows before install.',
+    'author': 'Jinasena Agricultural Machinery (Pvt) Ltd.',
+    'category': 'Extra Tools',
+    'license': 'LGPL-3',
+    'depends': [
+        'seed_master_data_and_settings',
+        'BugFix-Studio-Misc',
+        'bank-data',
+        'studio_usermodel_migration',
+    ],
+    'data': [
+        'data/res_currency.csv',
+        'data/res_currency_rate.csv',
+        'data/res_company.csv',
+        'data/res_bank.csv',
+        'data/res_partner_bank.csv',
+        'data/ir_sequence.csv',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
